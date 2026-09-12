952631122d1bf48c02f6d4e2aadd676df57a6aab
mirjamruk <mirzaambruk@gmail.com>
2026-09-12 17:34:55 +0700

    ui & fog

---
+++ scripts/fog.gd
@@ -0,0 +1,46 @@
+extends TileMapLayer
+
+@export var reveal_radius: int = 3
+
+var fog_tile := Vector2i(0, 0)
+
+var grid_width: int = 72
+var grid_height: int = 40
+
+
+func _ready() -> void:
+	fill_fog()
+
+
+func fill_fog() -> void:
+	clear()
+
+	for x in range(grid_width):
+		for y in range(grid_height):
+			var pos := Vector2i(x, y)
+
+			set_cell(
+				pos,
+				0,
+				fog_tile
+			)
+
+
+func reveal_area(center: Vector2i) -> void:
+	for x in range(
+		center.x - reveal_radius,
+		center.x + reveal_radius + 1
+	):
+		for y in range(
+			center.y - reveal_radius,
+			center.y + reveal_radius + 1
+		):
+			var pos := Vector2i(x, y)
+
+			if pos.x < 0 or pos.x >= grid_width:
+				continue
+
+			if pos.y < 0 or pos.y >= grid_height:
+				continue
+
+			erase_cell(pos)
