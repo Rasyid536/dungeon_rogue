4f13c118158b83f45d872b4ebec2ce6c0d5df91e
mirjamruk <mirzaambruk@gmail.com>
2026-09-13 13:30:26 +0700

    son

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,13 +1,25 @@
 extends Control
 
+# Panggil 2 node AudioStreamPlayer khusus UI
+@onready var sfx_start: AudioStreamPlayer = $VBoxContainer/AudioStreamPlayer2D
+@onready var sfx_quit: AudioStreamPlayer = $VBoxContainer/AudioStreamPlayer2D2
+
 func _on_start_pressed() -> void:
+	sfx_start.play()
+	await sfx_start.finished
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
+
+func _on_textStart_pressed() -> void:
+	sfx_start.play()
+	await sfx_start.finished
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
 
 func _on_quit_pressed() -> void:
+	sfx_quit.play()
+	await sfx_quit.finished
 	get_tree().quit()
 
-func _on_textStart_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/main.tscn")
-	
 func _on_textQuit_pressed() -> void:
+	sfx_quit.play()
+	await sfx_quit.finished
 	get_tree().quit()
