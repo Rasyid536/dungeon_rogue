a8c13a3ec936f48b23fe0274bf0307022f29d46e
mirjamruk <mirzaambruk@gmail.com>
2026-09-13 10:06:18 +0700

    Simpan perubahan lokal sebelum pull

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -9,3 +9,5 @@ func _on_quit_pressed() -> void:
 func _on_textStart_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
 	
+func _on_textQuit_pressed() -> void:
+	get_tree().quit()
